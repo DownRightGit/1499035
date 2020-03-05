f991f51e2016f5f7d750f7ebfda3fce6ec4473c1
DownRightGit <61540240+DownRightGit@users.noreply.github.com>
2020-03-05 21:35:29 +1100

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,11 @@
+#create a list of play options
+ans = ["Rock", "Paper", "Scissors"]
+
+#set player is False
+player = False
+
+#tell user to choose rock, paper, scissors
+msg = "What do you choose"
+
+#assign a random play to the computer
+computer = random.randint(0,2)
