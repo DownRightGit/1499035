51bda09882e073b7217d05f11f7cc9c846e53b9d
DownRightGit <61540240+DownRightGit@users.noreply.github.com>
2020-03-05 21:34:59 +1100

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,6 @@
+#Hangman
+#Generate Random word
+#Tell the user the number of letters in word
+#Ask the user to guess the letters
+#If the letter is correct let the user know
+#if the guess is wrong, than draw one part of the hangman 
